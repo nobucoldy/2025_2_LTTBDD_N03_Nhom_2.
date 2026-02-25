2dc9fbea607d17a896a76400807812c142aa8a5e
bac <quebac2385@gmail.com>
2026-02-25 15:05:38 +0700

    them icon menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main(List<String> args) {
   runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
@@ -9,15 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Planner App',
-          style: TextStyle(fontSize: 24, color: Colors.white),
-        ),
-        backgroundColor: Colors.purple,
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(icon: const Icon(Icons.menu), onPressed: () {}),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
