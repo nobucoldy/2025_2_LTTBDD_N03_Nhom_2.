ea508272a24a9e88655ecd678fbe6a25cc83f2dc
bac <quebac2385@gmail.com>
2026-03-03 23:17:17 +0700

    update

--- lib/widgets/add_plan_bottom_sheet.dart
+++ lib/widgets/add_plan_bottom_sheet.dart
@@ -14,7 +14,13 @@ class _AddPlanBottomSheetState extends State<AddPlanBottomSheet> {
       height: MediaQuery.of(context).size.height * 0.5,
       child: Column(
         children: [
-          TextField(),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Nhập kế hoạch mới tại đây',
+              border: InputBorder.none,
+            ),
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+          ),
           const SizedBox(height: 16),
           Row(children: [Icon(Icons.add_circle_outline_outlined)]),
         ],
