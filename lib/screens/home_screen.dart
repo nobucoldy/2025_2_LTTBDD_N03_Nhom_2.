0d966527c89a5cda237d1e2791001bea88132e2c
bac <quebac2385@gmail.com>
2026-03-04 15:12:50 +0700

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -32,7 +32,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    _selectedCategory = sampleCategories.first;
+    _selectedCategory = null;
   }
 
   Widget _buildMainContent() {
